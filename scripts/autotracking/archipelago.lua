8feed8bbc967b1d8667b58f9a8073b719ed9962d
Scipio Wright <scipiowright@gmail.com>
2023-04-30 21:49:49 -0400

    Finished numbering locations

--- scripts/autotracking/archipelago.lua
+++ scripts/autotracking/archipelago.lua
@@ -48,8 +48,7 @@ function onClear(slot_data)
         if name == "PathOption" then
             chosen_path = value
         elseif name == "ChestCount" then
-            --set_chest_count(value)
-            Tracker:FindObjectForCode("@Biome Checks/Overgrown Cavern/Chests").AvailableChestCount = value
+            set_chest_count(value)
         elseif name == "PedestalCount" then
             set_pedestal_count(value)
         elseif name == "OrbPath" then
@@ -58,50 +57,8 @@ function onClear(slot_data)
             boss_path = value
         end
     end
-end
-
-
--- called when an item gets collected
-function onItem(index, item_id, item_name, player_number)
-    if AUTOTRACKER_ENABLE_DEBUG_LOGGING_AP then
-        print(string.format("called onItem: %s, %s, %s, %s, %s", index, item_id, item_name, player_number, CUR_INDEX))
-    end
-    if index <= CUR_INDEX then
-        return
-    end
-    local is_local = player_number == Archipelago.PlayerNumber
-    CUR_INDEX = index;
-    local v = ITEM_MAPPING[item_id]
-    if not v then
-        if AUTOTRACKER_ENABLE_DEBUG_LOGGING_AP then
-            print(string.format("onItem: could not find item mapping for id %s", item_id))
-        end
-        return
-    end
-    if AUTOTRACKER_ENABLE_DEBUG_LOGGING_AP then
-        print(string.format("onItem: code: %s, type %s", v[1], v[2]))
-    end
-    if not v[1] then
-        return
-    end
-    local obj = Tracker:FindObjectForCode(v[1])
-    if obj then
-        if v[2] == "toggle" then
-            obj.Active = true
-        elseif v[2] == "progressive" then
-            if obj.Active then
-                obj.CurrentStage = obj.CurrentStage + 1
-            else
-                obj.Active = true
-            end
-        elseif v[2] == "consumable" then
-            obj.AcquiredCount = obj.AcquiredCount + obj.Increment
-        elseif AUTOTRACKER_ENABLE_DEBUG_LOGGING_AP then
-            print(string.format("onItem: unknown item type %s for code %s", v[2], v[1]))
-        end
-    elseif AUTOTRACKER_ENABLE_DEBUG_LOGGING_AP then
-        print(string.format("onItem: could not find object for code %s", v[1]))
-    end
+    local refresh = Tracker:FindObjectForCode("Refresh")
+    refresh.Active = not refresh.Active
 end
 
 
@@ -149,7 +106,7 @@ end
 -- add AP callbacks
 -- un-/comment as needed
 Archipelago:AddClearHandler("clear handler", onClear)
-Archipelago:AddItemHandler("item handler", onItem)
+--Archipelago:AddItemHandler("item handler", onItem)
 Archipelago:AddLocationHandler("location handler", onLocation)
 -- Archipelago:AddScoutHandler("scout handler", onScout)
 -- Archipelago:AddBouncedHandler("bounce handler", onBounce)
--- scripts/option_rules.lua
+++ scripts/option_rules.lua
@@ -22,10 +22,7 @@ end
 
 -- 0 == no bosses, 1 == main path, 2 == side path, 3 == main world
 function in_boss_set(boss_value)
-    print("boss value")
-    print(boss_value)
     if tonumber(boss_value) <= tonumber(boss_path) then
-        print("true")
         return true
     else
         print("false")
@@ -35,15 +32,37 @@ end
 
 
 function set_chest_count(value)
-    print("setting chest counts")
-    print(value)
     Tracker:FindObjectForCode("@Biome Checks/Overgrown Cavern/Chests").AvailableChestCount = value
+    Tracker:FindObjectForCode("@Biome Checks/Frozen Vault/Chests").AvailableChestCount = value
+    Tracker:FindObjectForCode("@Biome Checks/Mines/Chests").AvailableChestCount = value
+    Tracker:FindObjectForCode("@Biome Checks/Coal Pits/Chests").AvailableChestCount = value
+    Tracker:FindObjectForCode("@Biome Checks/Fungal Caverns/Chests").AvailableChestCount = value
     Tracker:FindObjectForCode("@Biome Checks/Snowy Depths/Chests").AvailableChestCount = value
+    Tracker:FindObjectForCode("@Biome Checks/Hiisi Base/Chests").AvailableChestCount = value
+    Tracker:FindObjectForCode("@Biome Checks/Underground Jungle/Chests").AvailableChestCount = value
+    Tracker:FindObjectForCode("@Biome Checks/Lukki Lair/Chests").AvailableChestCount = value
+    Tracker:FindObjectForCode("@Biome Checks/The Vault/Chests").AvailableChestCount = value
+    Tracker:FindObjectForCode("@Biome Checks/Temple of the Art/Chests").AvailableChestCount = value
+    Tracker:FindObjectForCode("@Biome Checks/The Tower/Chests").AvailableChestCount = value
+    Tracker:FindObjectForCode("@Biome Checks/Wizard's Den/Chests").AvailableChestCount = value
+    Tracker:FindObjectForCode("@Biome Checks/Power Plant/Chests").AvailableChestCount = value
 end
 
 
 function set_pedestal_count(value)
     Tracker:FindObjectForCode("@Biome Checks/Overgrown Cavern/Pedestals").AvailableChestCount = value
+    Tracker:FindObjectForCode("@Biome Checks/Frozen Vault/Pedestals").AvailableChestCount = value
+    Tracker:FindObjectForCode("@Biome Checks/Mines/Pedestals").AvailableChestCount = value
+    Tracker:FindObjectForCode("@Biome Checks/Coal Pits/Pedestals").AvailableChestCount = value
+    Tracker:FindObjectForCode("@Biome Checks/Fungal Caverns/Pedestals").AvailableChestCount = value
     Tracker:FindObjectForCode("@Biome Checks/Snowy Depths/Pedestals").AvailableChestCount = value
+    Tracker:FindObjectForCode("@Biome Checks/Hiisi Base/Pedestals").AvailableChestCount = value
+    Tracker:FindObjectForCode("@Biome Checks/Underground Jungle/Pedestals").AvailableChestCount = value
+    Tracker:FindObjectForCode("@Biome Checks/Lukki Lair/Pedestals").AvailableChestCount = value
+    Tracker:FindObjectForCode("@Biome Checks/The Vault/Pedestals").AvailableChestCount = value
+    Tracker:FindObjectForCode("@Biome Checks/Temple of the Art/Pedestals").AvailableChestCount = value
+    Tracker:FindObjectForCode("@Biome Checks/The Tower/Pedestals").AvailableChestCount = value
+    Tracker:FindObjectForCode("@Biome Checks/Wizard's Den/Pedestals").AvailableChestCount = value
+    Tracker:FindObjectForCode("@Biome Checks/Power Plant/Pedestals").AvailableChestCount = value
 end
 
